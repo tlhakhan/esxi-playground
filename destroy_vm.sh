916ce1e038e8247b9c105ddabb9206b5d1995c14
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-11-29 17:35:30 -0500

    better destroy.sh

--- destroy_vm.sh
+++ destroy_vm.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-VM_NAME=$1
-VM_ID=$(vim-cmd vmsvc/getallvms | grep $1 | awk '{print $1}')
+DATASTORE=$1
+VM_NAME=$2
+
+# check if folder already exists
+if [[ ! -d /vmfs/volumes/"$DATASTORE"/"$VM_NAME" ]]
+then
+        echo "Error:  Virtual machine does not exists."
+        exit 1
+fi
+
+VM_ID=$(vim-cmd vmsvc/getallvms | grep "$VM_NAME" | awk '{print $1}')
 
 if [[ ! -z "$VM_ID" ]] 
 then
@@ -9,10 +18,10 @@ then
 	vim-cmd vmsvc/unregister $VM_ID 
 	if [[ $? -eq 0 ]]
 	then
-		echo "Successfully unregistered $VM_NAME"
-		rm -rf $VM_NAME && echo "Successfully destroyed $VM_NAME"
+		echo "Info:  Successfully unregistered $VM_NAME"
+		rm -rf /vmfs/volumes/"$DATASTORE"/"$VM_NAME" && echo "Info: Successfully destroyed $VM_NAME"
 	fi
 else
-	echo "Error: unable to find $VM_NAME"
+	echo "Error:  Unable to find $VM_NAME"
 	exit 1
 fi
