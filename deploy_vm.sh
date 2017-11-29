368a4bbb380047522f8c20065466ddfb0aae12fc
root <root@centos-00.cloudcity.local>
2017-11-29 17:10:48 -0500

    updated deploy_vm for more usability

--- deploy_vm.sh
+++ deploy_vm.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
+# AUTHOR: tlhakhan
 # DESCRIPTION:
-# this script will create a virtual machine on an ESXi 6+ hypervisor.
-# it will create a small vm to specification from the vmx template config.
-# it will create a small thin provisioned disk with 8G as OS drive.
-# it will mount an ISO image and have the cdrom ready for install.
-#
-# pre-reqs: this script needs to be in the root-path of the vmfs datastore where you want to deploy vms.
-# pre-reqs: this script needs to have the cwd changed to basedir of this script.
+# This script will create a virtual machine on an ESXi 6+ hypervisor.
+# * It will create a VM to specification dictated from the vmx template heredoc. 
+# * It will create a small thin provisioned disk with 8G as OS drive.
+# * It will mount an ISO image and have the cdrom connected for OS install.
 #
 
 # USAGE:
-# ./deploy_vms [vm name]
+# ./deploy_vms [datastore name] [vm name] [ iso file ] [ guest os: centos7-64 | centos-64 | ]
 #
 
-# TODO:
-# extract basedir from the script source -- prefix basedir to paths in script below.
-# make configuration parameters into script arguments. 
-
 # configuration parameters
-VM_NAME="$1" # please no spaces
-GUEST_OS="centos-64" # centos for now
-ISO_IMAGE="/vmfs/volumes/5542cc0b-3ef7fae4-6b9b-00259055b5ab/software/CentOS-7-x86_64-Minimal-1511.iso" # path to iso file
+DATASTORE="$1"
+VM_NAME="$2" # please no spaces
+ISO_IMAGE="$3"
+GUEST_OS="${4:-centos7-64}" # default is centos7-64
+
+# check arg length is 4
+if [[ $# -ne 4 ]] 
+then
+	echo "Usage: ./deploy_vms [datastore name] [vm name] [ iso file ] [ guest os: centos7-64 | centos-64 | ]"
+	exit 1
+fi
 
 # check if folder already exists
-if [[ -d "$VM_NAME" ]]
+if [[ -d /vmfs/volumes/"$DATASTORE"/"$VM_NAME" ]]
 then
         echo "Error:  virtual machine already exists."
         exit 1
@@ -32,9 +34,9 @@ fi
 
 # creating vm
 echo "Info:  creating virtual machine $VM_NAME"
-mkdir "$VM_NAME"
+mkdir /vmfs/volumes/"$DATASTORE"/"$VM_NAME"
 
-cat << EOF_vm_template > "$VM_NAME"/"$VM_NAME".vmx
+cat << EOF_vm_template > /vmfs/volumes/"$DATASTORE"/"$VM_NAME"/"$VM_NAME".vmx
 .encoding = "UTF-8"
 config.version = "8"
 virtualHW.version = "11"
@@ -90,17 +92,23 @@ EOF_vm_template
 
 # create misc files
 echo "Info:  Making misc files."
-touch "$VM_NAME"/"$VM_NAME".vmsd
+touch /vmfs/volumes/"$DATASTORE"/"$VM_NAME"/"$VM_NAME".vmsd
 
 # create virtual disks
 # size: 8g, type: file, diskformat: think
 echo "Info:  Making vmkd files."
-vmkfstools -c 8g -W file -d thin "$VM_NAME"/"$VM_NAME".vmdk
+vmkfstools -c 8g -W file -d thin /vmfs/volumes/"$DATASTORE"/"$VM_NAME"/"$VM_NAME".vmdk
 
 # register vm on vsphere
 echo "Info:  Registering $VM_NAME into vSphere."
-VM_ID=$(vim-cmd solo/registervm /vmfs/volumes/local-ssd/"$VM_NAME"/"$VM_NAME".vmx "$VM_NAME") # successful output is VMID
+
+VM_ID=$(vim-cmd solo/registervm /vmfs/volumes/"$DATASTORE"/"$VM_NAME"/"$VM_NAME".vmx "$VM_NAME") # successful output is VMID
 
 # power on the vm
 echo "Info:  Powering on $VM_NAME."
 vim-cmd vmsvc/power.on $VM_ID
+vim-cmd vmsvc/power.getstate $VM_ID
+
+# list of all vms
+echo "Info:  List of all VMs."
+vim-cmd vmsvc/getallvms
