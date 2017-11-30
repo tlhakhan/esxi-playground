58175f9bf58dd7f6a015640da0ab17b3c9b84a31
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-11-29 19:02:08 -0500

    robust reset_vm.sh script

--- reset_vm.sh
+++ reset_vm.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
-vim-cmd vmsvc/snapshot.revert 58 3 false
-vim-cmd vmsvc/snapshot.revert 59 2 false
+VM_NAME=$1
+
+if [[ $# -ne 1 ]]
+then
+	echo "Usage ./reset_vm.sh [ vm name ]"
+	exit 1
+fi
+
+VM_ID=$(vim-cmd vmsvc/getallvms | grep "$VM_NAME" | awk '{print $1}')
+LAST_SNAP_ID=$(vim-cmd vmsvc/snapshot.get $VM_ID | grep Id | tail -n 1 | awk '{print $NF}')
+
+if [[ -z "$LAST_SNAP_ID" ]]
+then
+	echo "Warn:  Unable to find a snapshot on the virtual machine."
+	echo "Info:  Creating an initial snapshot."
+	vim-cmd vmsvc/snapshot.create $VM_ID snap0 snap0 True True
+	vim-cmd vmsvc/snapshot.get $VM_ID
+	exit 1	
+else
+	echo "Info:  Reverting VM to most recent snapshot."
+	vim-cmd vmsvc/snapshot.revert $VM_ID $LAST_SNAP_ID false
+fi
